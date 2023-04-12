2f53fd76da60f8bb365dcaf2caa6d6929f54584d
Olivier7355 <108932082+Olivier7355@users.noreply.github.com>
2023-04-12 08:37:58 +0000

    Define the App structure

---
+++ main.py
@@ -0,0 +1,15 @@
+# Import FastAPI & SQLAlchemy
+
+# Create the database (contacts.db)
+
+# Create the database table (id, name, phone_number, email)
+
+# Define the get endpoint list_all_contacts (/contacts/)
+
+# Define the get endpoint read_contact (/contacts/{contact_id})
+
+# Define the post endpoint create_contact (/contacts/)
+
+# Define the put endpoint update_contact (/contacts/{contact_id})
+
+# Define the delete endpoint delete_contact (/contacts/{contact_id})
