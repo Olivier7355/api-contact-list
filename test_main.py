1151113b433a12e047d9f6b9b02946e3fe4c710e
Olivier7355 <108932082+Olivier7355@users.noreply.github.com>
2023-04-12 09:52:45 +0000

    Add the update_contact test

--- test_main.py
+++ test_main.py
@@ -29,6 +29,27 @@ def test_create_contact():
     assert contact is not None
 
 # Define the test_update_contact
+def test_update_contact():
+    
+    # Create a contact
+    contact_data = {
+        "name": "Rick Hochet",
+        "phone_number": "0987654321",
+        "email": "rickh@test.com"
+    }
+    response = client.post("/contacts/", json=contact_data)
+    assert response.status_code == 200
+
+    # Update the phone number
+    update_data = {"phone_number": "222222"}
+    response = client.put("/contacts/2", json=update_data)
+
+    # Check that the contact was updated successfully
+    assert response.status_code == 200
+    assert response.json() == {"message": "Contact with id 2 has been updated !"}
+    db = session()
+    contact = db.query(Contact).filter(Contact.name == "Rick Hochet").first()
+    assert contact.phone_number == "222222"
 
 # Define the test_delete_contact
 
