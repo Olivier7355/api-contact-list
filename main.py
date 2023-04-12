2c80773ff02ad293a89d73891323ef69d0a3305d
Olivier7355 <108932082+Olivier7355@users.noreply.github.com>
2023-04-12 08:45:22 +0000

    Create the database & table

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 # Import FastAPI & SQLAlchemy
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+import re
 
 # Create the database (contacts.db)
+engine = create_engine('sqlite:///./contacts.db')
+session = sessionmaker(bind=engine)
+Base = declarative_base()
 
 # Create the database table (id, name, phone_number, email)
+class Contact(Base):
+    __tablename__ = 'contacts'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(40))
+    phone_number = Column(String(20))
+    email = Column(String(40))
+
+Base.metadata.create_all(engine)
 
 # Define the get endpoint list_all_contacts (/contacts/)
 
